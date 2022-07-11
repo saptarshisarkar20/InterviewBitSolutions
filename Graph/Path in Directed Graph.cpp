cccae9e906b94d73f07b098a876b5ada417ef67b
Saptarshi Sarkar <67330269+saptarshisarkar20@users.noreply.github.com>
2022-07-12 01:03:18 +0530

    see my sol

--- Graph/Path in Directed Graph.cpp
+++ Graph/Path in Directed Graph.cpp
@@ -1,48 +1,30 @@
-#include "bits/stdc++.h"
-using namespace std;
-class Solution
-{
-public:
-    int solve(int A, vector<vector<int>> &B);
-};
 
-unordered_map<int, vector<int>> createAdjacencyList(vector<vector<int>> &B)
-{
-    unordered_map<int, vector<int>> adj;
-    for (int i = 0; i < B.size(); i++)
-    {
-        int a = B[i][0];
-        int b = B[i][1];
-        adj[a].push_back(b);
+
+
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    vector<bool> vis(A+1, false);
+    vector<int> adj[A+1];
+    
+    for(auto it:B){
+        adj[it[0]].push_back(it[1]);
     }
-    return adj;
-}
-bool bfs(int A, unordered_map<int, vector<int>> &adj)
-{
-    unordered_map<int, bool> visited;
-    int front = 1;
-    queue<int> q;
-    q.push(front);
-    visited[front] = true;
-    while (!q.empty())
-    {
-        int curr = q.front();
-        if (curr == A)
-            return true;
-        q.pop();
-        for (auto n : adj[curr])
-        {
-            if (!visited[n])
-            {
-                q.push(n);
-                visited[n] = true;
+    
+    stack<int> st;
+    st.push(1);
+    vis[1]=true;
+    
+    while(!st.empty()){
+        int x = st.top();
+        st.pop();
+        for(auto it:adj[x]){
+            if(!vis[it]){
+                st.push(it);
+                vis[it]=true;
             }
         }
     }
-    return false;
-}
-int Solution::solve(int A, vector<vector<int>> &B)
-{
-    unordered_map<int, vector<int>> adj = createAdjacencyList(B);
-    return bfs(A, adj);
+    
+    return vis[A];
+    
 }
